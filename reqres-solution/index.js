09e5caf1a67f6479fd8c042f81aa0aa23e91667f
jordan <jharrison1@qa.com>
2022-10-04 12:46:58 +0100

    basic solution with a bit of bootstrap

--- reqres-solution/index.js
+++ reqres-solution/index.js
@@ -4,19 +4,21 @@ function writeOutput(newOutput) {
     document.getElementById("output").innerText = newOutput;
 }
 
+// JSON.stringify(data, null, 2) -> Converts an object to a JSON string with (2) spaces which
+// makes formatting a lot nicer
 function getUsers() {
     axios.get("https://reqres.in/api/users")
             .then(res => writeOutput(JSON.stringify(res.data.data, null, 2)))
             .catch(err => console.error(err));
 }
-console.log("YO!");
 
-function createUser() {
-    const name = document.getElementById("userName").value;
-    console.log("NAME:", name);
+function createUser(e) {
+    e.preventDefault(); // stops the page from reloading
+    const name = document.getElementById("userName").value; // get name from input
+    const job = document.getElementById("userJob").value; // get job from input
     axios.post("https://reqres.in/api/users", {
-        name: "Jordan",
-        "job": "trainer",
+        name,
+        job
     }).then(res => writeOutput(JSON.stringify(res.data, null, 2)))
         .catch(err => console.error(err));
 }
