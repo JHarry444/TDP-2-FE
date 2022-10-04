5936d8030ddf0cfc6c0834d933e5dbb792a15ab5
jordan <jharrison1@qa.com>
2022-10-04 12:26:39 +0100

    added functions and basic output

--- reqres-solution/index.js
+++ reqres-solution/index.js
@@ -1,8 +1,12 @@
 'use strict';
 
+function writeOutput(newOutput) {
+    document.getElementById("output").innerText = newOutput;
+}
+
 function getUsers() {
     axios.get("https://reqres.in/api/users")
-            .then(res => console.log(res))
+            .then(res => writeOutput(JSON.stringify(res.data.data, null, 2)))
             .catch(err => console.error(err));
 }
 console.log("YO!");
@@ -11,8 +15,24 @@ function createUser() {
     const name = document.getElementById("userName").value;
     console.log("NAME:", name);
     axios.post("https://reqres.in/api/users", {
-        name,
-        "job": "leader"
-    }).then(res => console.log(res))
+        name: "Jordan",
+        "job": "trainer",
+    }).then(res => writeOutput(JSON.stringify(res.data, null, 2)))
+        .catch(err => console.error(err));
+}
+
+function registerUser() {
+    axios.post("https://reqres.in/api/register", {
+        "email": "eve.holt@reqres.in",
+        "password": "pistol"
+    }).then(res => writeOutput(JSON.stringify(res.data, null, 2)))
+        .catch(err => console.error(err));
+}
+
+function loginUser() {
+    axios.post("https://reqres.in/api/login", {
+        "email": "eve.holt@reqres.in",
+        "password": "pistol"
+    }).then(res => writeOutput(JSON.stringify(res.data, null, 2)))
         .catch(err => console.error(err));
 }
